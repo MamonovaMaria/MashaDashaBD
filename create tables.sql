0bb35dc7b028938aabff97222e6fdfe2273c4ffd
Maria <mmamonova95@gmail.com>
2017-02-17 19:29:59 +0300

    changed create and added inserting

--- create tables.sql
+++ create tables.sql
@@ -1,23 +1,23 @@
-drop table if exists students ;
-drop table if exists subjects;
-drop table if exists exam;
+#drop table if exists students ;
+#drop table if exists subjects;
+#drop table if exists exam;
 
-CREATE TABLE students (
+CREATE TABLE if not exists students (
     student_id VARCHAR(6) PRIMARY KEY,
     firstname VARCHAR(20) NOT NULL,
     surname VARCHAR(20) NOT NULL,
     lastname VARCHAR(20) NOT NULL,
     group_num VARCHAR(2) NOT NULL,
-    phone_num INT(12) UNSIGNED NOT NULL
+    phone_num VARCHAR(12) NOT NULL
 );
 
-CREATE TABLE subjects (
+CREATE TABLE if not exists subjects (
     subject_id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    subject_name VARCHAR(20) NOT NULL,
-    teacher_name VARCHAR(20) NOT NULL
+    subject_name VARCHAR(50) NOT NULL,
+    teacher_name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE exam (
+CREATE TABLE if not exists exam (
     student_id VARCHAR(6),
     subject_id TINYINT UNSIGNED,
     mark VARCHAR(4) NOT NULL,
